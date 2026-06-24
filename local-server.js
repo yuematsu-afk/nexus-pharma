96afc57fa96ea40cac85dd5075894ad51f5fab10
yuematsu-afk <y.uematsu@sasa-eru.com>
2026-06-24 13:39:50 +0900

    Improve pharma M&A marketing site

---
+++ local-server.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const port = Number(process.env.PORT || 4176);
+const host = "127.0.0.1";
+
+const types = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+};
+
+const server = http.createServer((req, res) => {
+  const urlPath = decodeURIComponent((req.url || "/").split("?")[0]);
+  const relative = urlPath === "/" ? "index.html" : urlPath.replace(/^\/+/, "");
+  const resolved = path.resolve(root, relative);
+
+  if (!resolved.startsWith(root)) {
+    res.writeHead(403);
+    res.end("Forbidden");
+    return;
+  }
+
+  fs.readFile(resolved, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Not found");
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": types[path.extname(resolved).toLowerCase()] || "application/octet-stream",
+      "Cache-Control": "no-store",
+    });
+    res.end(data);
+  });
+});
+
+server.listen(port, host, () => {
+  console.log(`NexusM&A.Pharma local server: http://${host}:${port}/`);
+});
